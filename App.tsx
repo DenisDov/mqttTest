45f03a61976b29b058d8fc9377993c83ffef8e19
Denys Dovzhenko <dovzdenis@gmail.com>
2024-11-18 18:21:34 +0200

    rm unused code

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 
 import MqttComponent from './src/components/MqttComponent';
 
--- src/components/MqttComponent.js
+++ src/components/MqttComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {Text, Button} from 'react-native';
 import {useMqttClient} from '../hooks/use-mqqt';
 
 const MqttComponent = () => {
