9a0fece14e7e13deaf1f00a6ffed5cbf96893f03
Denys Dovzhenko <dovzdenis@gmail.com>
2024-11-18 18:13:56 +0200

    status

--- src/components/MqttComponent.js
+++ src/components/MqttComponent.js
@@ -3,11 +3,11 @@ import {View, Text, Button} from 'react-native';
 import {useMqttClient} from '../hooks/use-mqqt';
 
 const MqttComponent = () => {
-  const client = useMqttClient();
+  const {client, status} = useMqttClient();
 
   return (
     <View>
-      <Text>MQTT Client is running...</Text>
+      <Text>MQTT Client is {status}</Text>
       <Button
         title="SEND COORDINATES"
         onPress={() => client.publish('gps/tracker', 'lat:123,lon:123')}
--- src/hooks/use-mqqt.js
+++ src/hooks/use-mqqt.js
@@ -1,16 +1,19 @@
 import {useEffect, useState} from 'react';
 import {connect} from 'mqtt';
 
+//https://www.mqtt-dashboard.com/
 const mqttUrl = 'ws://broker.hivemq.com:8000/mqtt';
 
 export const useMqttClient = () => {
   const [client, setClient] = useState(null);
+  const [status, setStatus] = useState('disconnected');
 
   useEffect(() => {
     const mqttClient = connect(mqttUrl);
 
     mqttClient.on('connect', () => {
       console.log('Connected to MQTT broker');
+      setStatus('connected');
 
       const subscribeToTopic = topic => {
         mqttClient.subscribe(topic, err => {
@@ -35,22 +38,32 @@ export const useMqttClient = () => {
 
     mqttClient.on('error', error => {
       console.error('MQTT Error:', error);
+      setStatus('error');
     });
 
     mqttClient.on('offline', () => {
       console.warn('MQTT client is offline');
+      setStatus('offline');
     });
 
     mqttClient.on('reconnect', () => {
       console.log('Reconnecting to MQTT broker');
+      setStatus('reconnecting');
     });
 
     mqttClient.on('close', () => {
       console.log('MQTT connection closed');
+      setStatus('closed');
+    });
+
+    mqttClient.on('disconnect', () => {
+      console.log('Disconnected from MQTT broker');
+      setStatus('disconnected');
     });
 
     mqttClient.on('end', () => {
       console.log('MQTT connection ended');
+      setStatus('ended');
     });
 
     setClient(mqttClient);
@@ -60,5 +73,5 @@ export const useMqttClient = () => {
     };
   }, []);
 
-  return client;
+  return {client, status};
 };
